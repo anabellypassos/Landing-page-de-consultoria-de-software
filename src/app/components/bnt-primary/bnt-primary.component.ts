43d1db8141d11eb4c1b62efef94638f84d021981
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-06-13 19:37:43 -0300

    criação de botão

---
+++ src/app/components/bnt-primary/bnt-primary.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output,EventEmitter } from '@angular/core';
+
+type BtnVariants = "primary"| "secundary";
+@Component({
+  selector: 'app-bnt-primary',
+  imports: [],
+  templateUrl: './bnt-primary.component.html',
+  styleUrl: './bnt-primary.component.scss'
+})
+export class BntPrimaryComponent {
+@Input("btn-text") btnText: string = "";
+@Input()disabled: boolean = false;
+@Input()loading: boolean = false;
+@Input()variant : BtnVariants = "primary";
+@Output("submit")onSubmit = new EventEmitter();
+
+submit(){
+  this.onSubmit.emit();
+
+}
+}
