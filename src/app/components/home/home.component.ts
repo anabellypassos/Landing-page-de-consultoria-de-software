a2632539a20733bd056aa6c997bd4d0b415c6bac
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-07-15 20:48:44 -0300

    form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BntPrimaryComponent } from "../bnt-primary/bnt-primary.component";
+import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { BntPrimaryComponent } from "../bnt-primary/bnt-primary.component";
   imports: [
     HeaderComponent, NgOptimizedImage,
     BntPrimaryComponent,
+    NewsletterFormComponent
 ],
   providers:[],
   templateUrl: './home.component.html',
--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { BntPrimaryComponent } from '../bnt-primary/bnt-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
-  imports: [BntPrimaryComponent],
+  standalone: true, // Add this flag
+  imports: [BntPrimaryComponent, ReactiveFormsModule],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
+  newsletterForm: FormGroup; // No need for '!' if initialized in the constructor
+
+  constructor() {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+
 
 }
