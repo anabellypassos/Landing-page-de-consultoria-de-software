41940e71f3eb3df4f7f6ebf7d17d7d996a59f054
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-07-16 12:06:30 -0300

    conexão com api

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,24 +1,52 @@
-import { Component } from '@angular/core';
-import { BntPrimaryComponent } from '../bnt-primary/bnt-primary.component';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BntPrimaryComponent } from '../bnt-primary/bnt-primary.component';
+import { NewsletterService } from '../../services/newsletter.service';
+import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'app-newsletter-form',
-  standalone: true, // Add this flag
-  imports: [BntPrimaryComponent, ReactiveFormsModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BntPrimaryComponent
+  ],
+  providers:[
+    NewsletterService
+  ],
   templateUrl: './newsletter-form.component.html',
-  styleUrl: './newsletter-form.component.scss'
+  styleUrls: ['./newsletter-form.component.scss']
 })
 export class NewsletterFormComponent {
-  newsletterForm: FormGroup; // No need for '!' if initialized in the constructor
+  newsletterForm!: FormGroup;
+  loading = signal(false)
 
-  constructor() {
+  constructor(private service: NewsletterService ) {
     this.newsletterForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
 
+  // Método de submissão com o nome correto (onSubmit)
+  onSubmit() {
+    this.loading.set(true)
+    if (this.newsletterForm.valid) {
+      console.log('Formulário Válido! Enviando dados:', this.newsletterForm.value);
+      this.service.sendData(this.newsletterForm.value.name,this.newsletterForm.value.email).subscribe({
+       next: ()=>{
+            this.newsletterForm.reset();
+            this.loading.set(false);
+       }
 
+      })
 
+    } else {
+      console.error('Formulário inválido. Por favor, verifique os campos.');
+      // Opcional: Marcar campos como "tocados" para exibir mensagens de erro
+      this.newsletterForm.markAllAsTouched();
+    }
+  }
 }
